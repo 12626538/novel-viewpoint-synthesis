605e4f203539c112602466c8f2b6ad1c4429c259
12626538 <jip.greven@student.uva.nl>
2024-02-09 18:09:51 +0100

    account for different desktops

--- scripts/train_3du_data_2.sh
+++ scripts/train_3du_data_2.sh
@@ -1,9 +1,20 @@
 #!/bin/sh
-cd ~/novel-viewpoint-synthesis/
-python3 train.py \
-    -s ~/data1/3du_data_2 \
+if [ -d "~/novel-viewpoint-synthesis/" ]
+then
+    CODE_DIR="~/novel-viewpoint-synthesis/";
+    DATA_DIR="~/data1/";
+elif [ -d "/media/jip/T7/thesis/code/novel-viewpoint-synthesis/" ]
+then
+    CODE_DIR="/media/jip/T7/thesis/code/novel-viewpoint-synthesis/";
+    DATA_DIR="/media/jip/T7/thesis/code/data/";
+fi
+
+python3 $CODE_DIR/train.py \
+    -s "$DATA_DIR/3du_data_2" \
     --rescale 1 \
-    --device cuda:1 \
-    --num-points 30000 \
-    --densify-every 300 \
-    --grad-threshold 1e-4
+    --device cuda:0 \
+    --sh-degree 2 \
+    --num-points 10000 \
+    --test-at 5000 7000 11000 14000 17000 20000 25000 30000 \
+    --save-at 7000 14000 20000 25000 30000 \
+    --max-screen-size 50
--- scripts/train_bicycle.sh
+++ scripts/train_bicycle.sh
@@ -1,7 +1,18 @@
 #!/bin/sh
-cd ~/novel-viewpoint-synthesis/
-python3 train.py \
-    -s ~/data1/bicycle \
+if [ -d "~/novel-viewpoint-synthesis/" ]
+then
+    CODE_DIR="~/novel-viewpoint-synthesis/";
+    DATA_DIR="~/data1/";
+elif [ -d "/media/jip/T7/thesis/code/novel-viewpoint-synthesis/" ]
+then
+    CODE_DIR="/media/jip/T7/thesis/code/novel-viewpoint-synthesis/";
+    DATA_DIR="/media/jip/T7/thesis/code/data/";
+fi
+
+python3 $CODE_DIR/train.py \
+    -s "$DATA_DIR/bicycle" \
     --rescale 4 \
-    --device cuda:1 \
-    --sh-degree 0
+    --device cuda:0 \
+    --sh-degree 0 \
+    --test-at 5000 7000 11000 14000 17000 20000 25000 30000 \
+    --save-at 7000 14000 20000 25000 30000 \
