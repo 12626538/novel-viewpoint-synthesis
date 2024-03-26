4a6677fede824f3d472f0e0c8697c3b521163e0d
12626538 <jip.greven@student.uva.nl>
2024-03-26 13:26:33 +0100

    update

--- scripts/train_3du_data_8.sh
+++ scripts/train_3du_data_8.sh
@@ -15,14 +15,15 @@ python3 $CODE_DIR/train.py \
     --device cuda:0 \
     --test-at 5000 7000 11000 14000 17000 20000 25000 30000 \
     --save-at 7000 15000 22000 30000 \
-    --lr-scales 0.005 \
     --densify-from 500 \
     --densify-every 100 \
     --reset-opacity-every 3000 \
-    --grad-threshold 0.0005 \
-    --min-opacity 0.1 \
-    --max-screen-size 0.10 \
+    --grad-threshold 0.0002 \
     --max-density 0.05 \
-    --loss-weight-mae 0.8 \
+    --min-opacity 0.1 \
+    --max-screen-size 0.15 \
+    --loss-weight-mse 1.0 \
     --loss-weight-dssim 0.2 \
+    --loss-weight-lpips 0.1 \
+    --loss-weight-mae 0.8 \
     --block-width 14
